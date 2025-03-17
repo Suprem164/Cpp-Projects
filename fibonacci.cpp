8b9639c473f5b34e16387ecdc96bd4ab1af4d3a3
Lawarna Aree <56360110+Suprem164@users.noreply.github.com>
2025-03-17 19:48:10 +0545

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-    int a = 0, b = 1, c = 1, d, input;
+    int a = 0, b = 1, c = 1, d, input; //initializing the variables 
 
-    cout << "Enter the range: ";
+    cout << "Enter the range: "; //Taking series range from user
     cin >> input;
 
-    if (input <= 2)
+    if (input <= 2) //First 3 values are already printed so, user should enter range >= 3
     {
         cout << "Please enter positive number greater than 2.";
         return 1;
     }
 
-    cout << "Fibonacci series: " << a << " " << b << " " << c;
+    cout << "Fibonacci series: " << a << " " << b << " " << c; //Printing 0 1 1
 
-    for (int i = 3; i < input; i++)
+    for (int i = 3; i < input; i++) //initializing the range
     {
-        d = b + c;
-        cout << " " << d;
-        b = c;
-        c = d;
+        d = b + c; //adding previous terms
+        cout << " " << d; //printing main values
+        b = c; //initializing b as c 
+        c = d; //initializing c as d 
     }
     return 0;
 }
